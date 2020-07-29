a7558689dfd61413ddc0d450b41437c616551367
BulletSentence <leonardo.a@acad.ifma.edu.br>
2020-07-29 07:10:15 -0300

    Integer Division

--- std01.py
+++ std01.py
@@ -3,9 +3,18 @@ import sys
 print('Hello, Python!')
 print(sys.version)
 
+# Types
 print(type(12))
 print(type(1.14))
 print(type("HI"))
 
+# Convertion
 print(int(1.2))
 print(float(30))
+
+# Division
+print(12/5)
+
+# Integer Division
+print(12//5)
+
